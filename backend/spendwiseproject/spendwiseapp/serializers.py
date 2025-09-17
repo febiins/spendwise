1a98961c784114064f22bfc1986ca420d076e827
febin <febinsunny2004@gmail.com>
2025-09-17 10:07:35 +0530

    progressing with views and serializers

--- backend/spendwiseproject/spendwiseapp/serializers.py
+++ backend/spendwiseproject/spendwiseapp/serializers.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from .models import Category, Expense
 from django.contrib.auth.models import User
 
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model= User
+        fields=['username','password']
+    def create(self,validate_data):
+        user=User.objects.create_user(username=validate_data['username'],password=validate_data['password'])
+        return user
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
--- backend/spendwiseproject/spendwiseapp/views.py
+++ backend/spendwiseproject/spendwiseapp/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-from rest_framework import viewsets, permissions
+ffrom rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from .serializers import RegisterSerializer
 from .models import Category, Expense
 from .serializers import CategorySerializer, ExpenseSerializer
+
+class RegisterView(APIView):
+    permission_classes=[AllowAny]
+
+    def post(self,request):
+        serializer= RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user=serializer.save()
+            if user:
+                refresh=RefreshToken.for_user(user)
+                return Response({
+                    "user":user.username,
+                   " refresh":str(refresh),
+                   "access": str(refresh.access_token),
+                },status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -25,4 +50,10 @@ class ExpenseViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+class LoginView(TokenObtainPairView):
+    permission_classes=[AllowAny]        
+
+
+class RefreshToken(TokenRefreshView):
+    permission_class=[AllowAny]
 
